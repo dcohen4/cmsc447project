48f4053b7f2ffa788d7f0efb9649589e2f236098
Hamza Ansari <hansari2@umbc.edu>
2023-11-30 00:27:59 -0500

    Fixed json-server issue

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,54 +1,55 @@
+'use client';
+
+
+import React, { useEffect, useState } from 'react';
 import { getAllTodos } from "@/api";
 import { TodoList } from "@/components/TodoList";
-import React, {useEffect, useState} from "react";
 import AddTask from "@/components/AddTask";
-
 import ViewTask from "@/components/ViewTask";
 import Dateshifter from "@/components/Dateshifter";
+import { ITask } from '@/types/types'
 
 
+export default function Home() {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  // const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState<ITask[]>([]);
 
-export default async function Home() {
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const allTasks = await getAllTodos();
 
-  // loop through all of todos.json and call transferTodos on each, checking the date, and returning if the date == true
-  // while (todos.json != NULL) {
-          
-  //}
+      const formatDateOnly = (date: Date) => date.toISOString().split('T')[0];
+      const filteredTasks = allTasks.filter(task => 
+      formatDateOnly(new Date(task.date)) === formatDateOnly(selectedDate)
+);
+      setTasks(filteredTasks);
+    };
 
-  const tasks = await getAllTodos();
-  
-  const currentDate = new Date();
-  const formattedDate = currentDate.toDateString();
-
+    fetchTasks();
+  }, [selectedDate]);
 
   return (
     <main className="max-w-4xl mx-auto mt-15">
-
-
       <div className="text-left my-10 ">
-        <h1 className="font-bold text-lg">Task Management App: {formattedDate}</h1>
-        <Dateshifter />
+        <h1 className="font-bold text-lg">Task Management App: {selectedDate.toDateString()}</h1>
+        <Dateshifter onDateChange={(date) => setSelectedDate(date)} />
       </div>
 
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '25vh' }}>
         <h3 className="font-bold text-lg"></h3>
         <div style={{ display: 'flex', justifyContent: 'space-between', width: '150%', margin: '150px' }}>
           <div className='mb-8' style={{ flex: 2, marginRight: '200px' }}>
-            <TodoList tasks={tasks} />
+            <TodoList tasks={tasks} /> {/* Display only tasks for the selected date */}
           </div>
-
 
           <div className='grid justify-evenly grid-cols-2 gap-16'>
-
             <AddTask></AddTask>
             <ViewTask></ViewTask>
-            
             {/* <EditTask></EditTask>
-            <DeleteTask></DeleteTask> */}
+                <DeleteTask></DeleteTask> */}
           </div>
         </div>
-
-
       </div>
     </main>
   )
--- my-app/components/Dateshifter.tsx
+++ my-app/components/Dateshifter.tsx
@@ -1,28 +1,37 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, Dispatch, SetStateAction } from 'react';
 
-const Dateshifter = () => {
+interface DateshifterProps {
+  onDateChange: Dispatch<SetStateAction<Date>>;
+}
+
+const Dateshifter = ({ onDateChange }: DateshifterProps) => {
   const [currentDay, setCurrentDay] = useState(new Date());
+
+  const updateDay = (newDay: Date) => {
+    setCurrentDay(newDay);
+    onDateChange(newDay); // Call the callback with the new date
+  };
 
   const incrementDay = () => {
     const nextDay = new Date(currentDay);
-    nextDay.setDate(currentDay.getDate() + 1);
-    setCurrentDay(nextDay);
+    nextDay.setUTCDate(currentDay.getUTCDate() + 1);
+    updateDay(nextDay);
   };
-
+  
   const decrementDay = () => {
     const prevDay = new Date(currentDay);
-    prevDay.setDate(currentDay.getDate() - 1);
-    setCurrentDay(prevDay);
+    prevDay.setUTCDate(currentDay.getUTCDate() - 1);
+    updateDay(prevDay);
   };
+  
 
   return (
     <div>
       <h1>Current Day: {currentDay.toDateString()}</h1>
       <button onClick={decrementDay}>Previous Day</button>
       <button onClick={incrementDay}>Next Day</button>
-      
     </div>
   );
 };
