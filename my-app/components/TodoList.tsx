326c92bd2d91fa8ac0f171bdaaba6c45d8aefb3e
DLCohen98 <daniel.cohen335@gmail.com>
2023-12-08 21:15:20 -0500

    updated format

--- my-app/components/TodoList.tsx
+++ my-app/components/TodoList.tsx
@@ -9,13 +9,8 @@ interface TodoListProps{
 
 export const TodoList: React.FC<TodoListProps> = ({tasks}) => {
   return (
-<<<<<<< Updated upstream
-    <div className="relative overflow-x-auto max-w-[690px] shadow-md sm:rounded-lg md:block">
-    <table className="overflow-hidden w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-=======
     <div className="relative hover:border-double overflow-x-auto max-h-[700px] shadow-xl sm:rounded-lg md:block">
     <table className="overflow-hidden w-full text-sm text-left rtl:text-right ">
->>>>>>> Stashed changes
     {/* head */}
     <thead className="text-xs uppercase">
       <tr>
