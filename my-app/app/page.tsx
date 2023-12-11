9a684d22332b3ac960a3ed6a7670651d4897b328
DLCohen98 <daniel.cohen335@gmail.com>
2023-12-11 12:05:12 -0500

    removed unnecessary display

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
                 <AddTask />
                 <EditTask onDateSelect={handleSelectedDate} />
                
-                {calendarDate && <p>Selected Date: {calendarDate.toDateString()}</p>}
+                
               </div>
             </div>
           </div>
