cf8931967e434dafa06957d4c27d19d24f738535
Hamza Ansari <hansari2@umbc.edu>
2023-12-11 23:00:16 -0500

    Fixing HomePage

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,78 +1,238 @@
-'use client';
+// "use client"
 
-import React, { useEffect, useState } from 'react';
-import { getAllTodos } from "@/api";
-import { TodoList } from "@/components/TodoList";
-import AddTask from "@/components/AddTask";
-import Dateshifter from "@/components/Dateshifter";
-import { ITask } from '@/types/types';
-import './globals.css';
-import Dropdown from '@/components/Dropdown';
+// import React, { useEffect } from 'react';
+// import TaskPage from './taskPage';
+// import Modal from 'react-modal';
+
+// const HomePage: React.FC = () => {
+//   const [modalIsOpen, setModalIsOpen] = React.useState(
+//     () => JSON.parse(localStorage.getItem('modalIsOpen') || 'false')
+//   );
+
+//   useEffect(() => {
+//     localStorage.setItem('modalIsOpen', JSON.stringify(modalIsOpen));
+//   }, [modalIsOpen]);
+
+//   const navigateToPage = () => {
+//     setModalIsOpen(true);
+//   };
+//   // const [showTaskPage, setShowTaskPage] = React.useState(false);
+
+//     // const navigateToPage = () => {
+//   //   setShowTaskPage(true);
+//   // };
+// //   return (
+// //     <div className="flex items-center justify-center h-screen bg-gray-100">
+// //       {showTaskPage ? <TaskPage /> : (
+// //         <div className="text-center">
+// //           <h1 className="text-5xl font-bold mb-6">
+// //             : <span className="text-blue-600">Open Calendar?</span>
+// //           </h1>
+// //           <p className="mb-10 text-gray-700">
+// //             By: Hamza, 
+// //           </p>
+// //           <button
+// //             onClick={navigateToPage}
+// //             className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out"
+// //           >
+// //             Enter!
+// //           </button>
+// //         </div>
+// //       )}
+// //     </div>
+// //   );
+// // };
+
+// return (
+//   <div className="flex items-center justify-center h-screen bg-gray-100">
+//     <div className="text-center">
+//       <h1 className="text-5xl font-bold mb-6">
+//         zzz <span className="text-blue-600">wonderful</span>
+//       </h1>
+//       <p className="mb-10 text-gray-700">
+//         zzz
+//       </p>
+//       <button
+//         onClick={navigateToPage}
+//         className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded-full transition duration-300 ease-in-out"
+//       >
+//         Open Page
+//       </button>
+//     </div>
+//     <Modal
+//       isOpen={modalIsOpen}
+//       onRequestClose={() => setModalIsOpen(false)}
+//       contentLabel="Task Page"
+//     >
+//       <TaskPage />
+//     </Modal>
+//   </div>
+// );
+// };
+// export default HomePage;
+
+// "use client"
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import TaskPage from './TaskPage';
+// import { ThemeProvider } from '@/context/ThemeContext';
+// import ClientThemeWrapper from '@/context/ClientThemeWrapper';
+// import Dropdown from '@/components/Dropdown';
+// import Tutorial from '@/components/Tutorial';
+
+// const HomePage: React.FC = () => {
+//   return (
+//     <ThemeProvider>
+//     <ClientThemeWrapper>
+    
+//     <div className=""><Dropdown /> </div>
+   
+    
+// <div className="flex flex-col items-center justify-center h-screen">
+//   <div className="text-center">
+//     <div className="flex justify-center items-center mb-4">
+//       <img src="/cal.png" alt="Logo" />
+//     </div>
+//     <h1 className="text-3xl font-bold mb-6">WELCOME TO THE TASKMANAGEMENT WEB APP!</h1>
+//     <p className="mb-6">Hamza, Nia, Safiya, Dan, Conor, Akhil.</p>
+//     <Link to="/tasks">
+//       <button
+//         className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+//       >
+//         Enter
+//       </button>
+//     </Link>
+//   </div>
+//   </div>
+
+// </ClientThemeWrapper>
+//       </ThemeProvider>
+
+//   );
+// };
+
+// const App: React.FC = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<HomePage />} />
+//         <Route path="/tasks" element={<TaskPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default App;
+
+
+'use client'
+import React, { useState, useEffect } from 'react';
+import Modal from 'react-modal';
+import TaskPage from './TaskPage';
 import { ThemeProvider } from '@/context/ThemeContext';
 import ClientThemeWrapper from '@/context/ClientThemeWrapper';
-import EditTask from '@/components/ViewTask';
-import SnowComponent from '@/components/SnowComponent';
+import Dropdown from '@/components/Dropdown';
 
-export default function Page() {
-
-  
-
-  const [selectedDate, setSelectedDate] = useState(new Date(localStorage.getItem('selectedDate') || Date.now()));
-  const [tasks, setTasks] = useState<ITask[]>([]);
-  const [calendarDate, setCalendarDate] = useState<Date | null>(null);
+const HomePage: React.FC = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(
+    () => JSON.parse(localStorage.getItem('modalIsOpen') || 'false')
+  );
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const allTasks = await getAllTodos();
+    localStorage.setItem('modalIsOpen', JSON.stringify(modalIsOpen));
+  }, [modalIsOpen]);
 
-      const formatDateOnly = (date: Date) => date.toISOString().split('T')[0];
-      const filteredTasks = allTasks.filter(task =>
-        formatDateOnly(new Date(task.date + 'T00:00:00Z')) === formatDateOnly(selectedDate)
-      );
-      console.log("Selected Date in Home:", selectedDate);
-
-      localStorage.setItem('selectedDate', selectedDate.toString());
-
-      setTasks(filteredTasks);
-    };
-    fetchTasks();
-  }, [selectedDate]);
-
-  const handleSelectedDate = (date: Date) => {
-    setCalendarDate(date); 
-    setSelectedDate(date); 
+  const openModal = () => {
+    setModalIsOpen(true);
   };
 
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  const customStyles = {
+    content: {
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      margin: '0',
+      padding: '0',
+      border: 'none',
+      background: 'none',
+      overflow: 'auto',
+      WebkitOverflowScrolling: 'touch',
+      borderRadius: '0',
+      outline: 'none',
+      position: 'absolute'
+    }
+  };
 
   return (
-    <main>
-      <ThemeProvider>
-        <ClientThemeWrapper>
-          <div className="h-screen">
-            <div><Dropdown /></div>
-            <div className='flex pt-8 my-8'>
-              <div className="container border-solid border-2 h-auto w-full pt-8 pl-8 ms-8 border-secondary">
-                <h1 className="text-primary font-bold text-2xl">{selectedDate.toDateString()}</h1>
-                {tasks.length === 0 ? (
-                  <div className="items-center h-full w-full flex justify-center text-primary text-5xl">
-                    Nothing to Do? 😴
-                  </div>
-                ) : (
-                  <TodoList tasks={tasks} />
-                )}
-              </div>
-              <div className='justify-items-center content-center place-content-evenly space-y-6 mx-8'>
-                <Dateshifter onDateChange={(date) => setSelectedDate(date)} />
-                <AddTask />
-                <EditTask onDateSelect={handleSelectedDate} />
-               
-                
-              </div>
+    <ThemeProvider>
+      <ClientThemeWrapper>
+        <div className=""><Dropdown /> </div>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <div className="text-center">
+            <div className="flex justify-center items-center mb-4">
+              <img src="/cal.png" alt="Logo" />
             </div>
+            <h1 className="text-3xl font-bold mb-6">WELCOME TO THE TASKMANAGEMENT WEB APP!</h1>
+            <p className="mb-6">Hamza, Nia, Safiya, Dan, Conor, Akhil.</p>
+            <button
+              onClick={openModal}
+              className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+            >
+              Enter
+            </button>
           </div>
-        </ClientThemeWrapper>
-      </ThemeProvider>
-      {/* <SnowComponent /> */}
-    </main>
-  )
-}
+          <Modal
+            isOpen={modalIsOpen}
+            onRequestClose={closeModal}
+            contentLabel="Task Page"
+            style={customStyles}
+          >
+            <TaskPage />
+          </Modal>
+        </div>
+      </ClientThemeWrapper>
+    </ThemeProvider>
+  );
+};
+
+export default HomePage;
+
+// 'use client'
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+// import TaskPage from './taskPage';
+
+// const HomePage: React.FC = () => {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={
+//           <div className="flex flex-col items-center justify-center h-screen bg-white">
+//             <div className="text-center">
+//               <div className="mb-4">
+//                 <img src="/path-to-your-logo.png" alt="Logo" />
+//               </div>
+//               <h1 className="text-3xl font-bold mb-6">WELCOME TO THE TASKMANAGEMENT WEB APP</h1>
+//               <p className="mb-6">Hamza, Nia, Safiya, Dan, Conor, Akhil.</p>
+//               <Link to="/tasks">
+//                 <button
+//                   className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out"
+//                 >
+//                   Enter
+//                 </button>
+//               </Link>
+//             </div>
+//           </div>
+//         } />
+//         <Route path="/tasks" element={<TaskPage />} />
+//       </Routes>
+//     </Router>
+//   );
+// };
+
+// export default HomePage;
